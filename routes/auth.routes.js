d4094e9b9aa9e09b2d21a607a0f38b1d0d1b0d49
bvalette <bvalette@student.42.fr>
2022-03-28 17:19:35 +0200

    fix email/username taken error message

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -52,7 +52,7 @@ router.post("/signup", isLoggedOut, (req, res) => {
     if (found) {
       return res
         .status(400)
-        .render("auth.signup", { errorMessage: "Email already taken." });
+        .render("auth/signup", { errorMessage: "Email already taken." });
     }
 
     // if user is not found, create a new user - start with hashing the password
