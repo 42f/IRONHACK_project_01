791c3750e8459a60ad0a458aaa2909ed87e37563
bvalette <bvalette@student.42.fr>
2022-03-29 12:01:50 +0200

    user library displayed

--- models/Link.model.js
+++ models/Link.model.js
@@ -1,11 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const linkSchema = new Schema({
-
   trackId: { type: Schema.Types.ObjectId, ref: "Track", required: true },
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true }
 });
 
-const Link = model("link", linkSchema);
+const Link = model("Link", linkSchema);
 
 module.exports = Link;
