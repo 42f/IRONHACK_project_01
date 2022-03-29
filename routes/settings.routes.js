56146ea5dcde27515be1b5d07f6264605249e907
bvalette <bvalette@student.42.fr>
2022-03-29 18:28:14 +0200

    callback for api

--- routes/settings.routes.js
+++ routes/settings.routes.js
@@ -1,4 +1,4 @@
-const { redirectSpotifyLogin } = require('../api/spotify-calls');
+const { getSpotifyToken, doImportSongs, redirectSpotifyLogin } = require('../api/spotify-calls');
 const router = require("express").Router();
 const isLoggedIn = require('../middleware/isLoggedIn')
 const Track = require('../models/Track.model')
@@ -15,7 +15,7 @@ router.get("/import", isLoggedIn, (req, res, next) => {
 router.get("/library", isLoggedIn, async (req, res, next) => {
   try {
     const tracklist = await req.user.getLinks();
-    res.render("settings/library", {tracklist});
+    res.render("settings/library", { tracklist });
   } catch (error) {
     console.log(error);
     next(error);
@@ -27,12 +27,32 @@ router.post("/library/create", isLoggedIn, (req, res, next) => {
   if (mySongs != 'on' && myPlaylists != 'on' && spotifyPlaylist != 'on') {
     res.redirect('/settings/import')
   } else {
+    req.session.playlistData = { mySongs, myPlaylists, spotifyPlaylist };
     next();
   }
 }, redirectSpotifyLogin);
 
-router.get("/library/callback", isLoggedIn, (req, res, next) => {
-  console.log('CALLBACK called by spotify ->', req.body);
+router.get("/library/callback", isLoggedIn, async (req, res, next) => {
+
+  const userCode = req.query.code;
+  const receivedstate = req.query.state;
+  const storedState = req.session.state;
+
+  // check state in cookie, if ok clear it, if not redirect
+  if (storedState !== receivedstate) {
+    console.error('Not the right state');
+    return res.status(400).send('wrong state');
+  }
+  delete req.session.state;
+
+  try {
+    const authToken = await getSpotifyToken(userCode);
+    await doImportSongs(req.user, req.session.playlistData, authToken)
+    res.redirect('/settings/library')
+  } catch (error) {
+    next(error);
+  }
+
 });
 
 module.exports = router;
--- utils/consts.js
+++ utils/consts.js
@@ -1,12 +1,17 @@
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/spotify-app";
 
+const PORT = process.env.PORT || 3000;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URL = process.env.REDIRECT_URI;
+const REDIRECT_URL = process.env.REDIRECT_URL;
+const SPOTIFY_AUTH_URL = process.env.SPOTIFY_AUTH_URL;
+const SPOTIFY_TOKEN_URL = process.env.SPOTIFY_TOKEN_URL;
 
 module.exports = {
 	MONGO_URI,
 	CLIENT_ID,
 	CLIENT_SECRET,
 	REDIRECT_URL,
+	SPOTIFY_AUTH_URL,
+	SPOTIFY_TOKEN_URL,
 };
