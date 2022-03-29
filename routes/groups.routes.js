3fc26ff684ac1a5229a1c0081dddf28fd8ba799a
Dev deRev <devderev@free.fr>
2022-03-29 09:56:17 +0200

    GroupRoutes-Changement

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -3,16 +3,6 @@ const isLoggedIn = require('../middleware/isLoggedIn');
 const router = require("express").Router();
 
 
-
-// const listMoviesRoute = (req) => `${req.baseUrl}`;
-// const newMovieRoute = (req) => `${req.baseUrl}/create`;
-
-// const listMovieView = `movies/movies`;
-// const detailsMovieView = `movies/movie-details`;
-// const newMovieView = `movies/new-movie`;
-// const editMovieView = `movies/edit-movie`;
-
-
 router.get('/', (req, res, next)=>{
 
     res.render('groups/allGroups')
