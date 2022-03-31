dff616573d72351cb67daa7304915e0e26ab7731
Dev deRev <devderev@free.fr>
2022-04-01 01:02:08 +0200

    Page showOneGroup and all groups

--- public/js/form-add-remove-user.js
+++ public/js/form-add-remove-user.js
@@ -1,18 +1,38 @@
 console.log('appel ok !');
-const usersList = document.querySelector('#users-list')
+// const usersList = document.querySelector('#users-list')
 // if ( elem.checked )
 // $input.is( ":checked" )
 
-usersList.addEventListener('click', (e)=>{
-    console.log(e.target.closest('.row-user').dataset.userid)
-    const userId = e.target.closest('.row-user').dataset.userid
-    const checkbox = document.getElementById(userId)
+// usersList.addEventListener('click', (e)=>{
+//     // console.log(e.target.closest('.row-user').dataset.userid)
+//     console.log(e.target)
 
-    if(checkbox.hasAttribute('checked')){
-        checkbox.removeAttribute('checked')
-    }else{
-        checkbox.setAttribute('checked', 'true')
-    }
+//     const userId = e.target.closest('div.row-user').dataset.userid
+//     const checkbox = document.getElementById(userId)
 
-})
+//     if(checkbox.hasAttribute('checked')){
+//         checkbox.removeAttribute('checked')
+//     }else{
+//         checkbox.setAttribute('checked', 'true')
+//     }
+
+// })
   
+const rowUser = document.querySelectorAll('.row-user')
+
+rowUser.forEach(row=>{
+    row.addEventListener('click', (e)=>{
+        console.log(e.target);
+        const userId = row.dataset.userid
+        console.log(userId, 'USER ID ON CLIIIIICK');
+        const checkbox = document.getElementById(userId)
+
+
+        if(checkbox.hasAttribute('checked')){
+            checkbox.removeAttribute('checked')
+        }else{
+            checkbox.setAttribute('checked', 'true')
+        }
+
+    })
+})
